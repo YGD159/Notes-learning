bb7a9f82bdc5b30f8ee8046ed09c6c5ecb0b2119
YGD <52145923+YGD159@users.noreply.github.com>
2021-06-24 14:26:00 +0800

    Add files via upload

---
+++ Protobuf资料/src/student_proto/student/CMakeFiles/student.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/student.dir/student.cpp.o"
+  "CMakeFiles/student.dir/student.pb.cc.o"
+  "../bin/student.pdb"
+  "../bin/student"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/student.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
